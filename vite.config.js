1e2b0120f53d1c528326c13b8636377f10f54603
BusraKahraman <busrakahraman.b@gmail.com>
2026-01-14 21:52:12 +0300

    Add VitePWA to make app downloadable and update gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/study-tracker/',
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: 'Study Timer',
+				short_name: 'StudyTimer',
+				description: 'Study timer with sessions and streaks',
+				theme_color: '#000000',
+				background_color: '#000000',
+				display: 'standalone',
+				start_url: '/',
+			},
+		}),
+	],
 });
